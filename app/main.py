7fadfdbf149703f99eb609fef773cf6615958f4b
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-07-24 09:27:18 -0700

    Add all endpoints to see in the docs

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from typing import Optional
 from app.routers.user_router import router as user_router
+from app.routers.meal_plan_router import router as meal_router
+from app.routers.activity_router import router as activity_router
+from app.routers.insulin_dose_router import router as insulin_dose_router
+from app.routers.glucose_reading_router import router as glucose_reading_router
+from app.routers.logs_router import router as logs_router
+from app.routers.dexcom_upload_router import router as dexcom_upload_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(meal_router)
+app.include_router(activity_router)
+app.include_router(insulin_dose_router)
+app.include_router(glucose_reading_router)
+app.include_router(logs_router)
+app.include_router(dexcom_upload_router)
 
 @app.get("/")
 def read_root():
